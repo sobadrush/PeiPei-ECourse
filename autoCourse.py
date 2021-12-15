7e87161db0155c64d7ad9cb40ae96ed926315674
sobadrush <sobadrush@icloud.com>
2021-12-15 21:07:03 +0800

    1. 加入log設定 2. 調整預設掛課時數為 01:15:00

--- autoCourse.py
+++ autoCourse.py
@@ -3,6 +3,7 @@
 import time
 import re
 import base64
+import logging
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -17,10 +18,11 @@ from myUtils import *
 # from myUtils import attendToCourse
 
 if __name__ == '__main__':
+    logger = initLogging(logging.DEBUG) # 初始化logging
 
-    acctUsername = input("請輸登入帳號：") or "tvbear8068"
+    acctUsername = input("請輸入登入帳號：") or "tvbear8068"
     acctPassword = base64.b64encode(input("請輸入登入密碼：").encode("UTF-8") ) or "SnVsbGllMjAwOTA4MjQ="
-    targetHHmmss = input("請輸入課程目標時數(格式: HH:mm:ss，不輸入預設為 01:00:00)：") or "01:00:00"
+    targetHHmmss = input("請輸入課程目標時數(格式: HH:MM:SS，不輸入預設為 01:15:00)：") or "01:15:00"
 
     options = Options()
     #options.add_argument("--disable-notifications")  # 取消所有的alert彈出視窗
@@ -48,36 +50,35 @@ if __name__ == '__main__':
     browser.execute_script('''document.querySelector("a[href='/mooc/profile.php']").click()''')
 
     # 選課清單
-    myCourseUrl="https://ups.moe.edu.tw/mooc/user/mycourse.php"
     curseTrList = browser.execute_script('''return document.querySelectorAll(".table.subject.row")[1].tBodies[0].children''')
     time.sleep(3)
-    print(f"curseTrList = {curseTrList}")
-    print(f"len(curseTrList) = {len(curseTrList)}")
+    logger.info(f"curseTrList = {curseTrList}")
+    logger.info(f"len(curseTrList) = {len(curseTrList)}")
 
     courseList = [] # 課程List
     for curseTr in curseTrList:
         tdArr = curseTr.find_elements(By.TAG_NAME, "td")
-        # print(f"tdArr : {tdArr}")
-        # print(f"tdArr[2] : {tdArr[2]}")
-        # print(f"tdArr[5] : {tdArr[5]}")
+        # logger.info(f"tdArr : {tdArr}")
+        # logger.info(f"tdArr[2] : {tdArr[2]}")
+        # logger.info(f"tdArr[5] : {tdArr[5]}")
         courseName = tdArr[2].text
-        accmulateTime = tdArr[5].text
-        print(f"courseName : {courseName}")
-        print(f"accmulateTime : {accmulateTime}")
+        accmulateTime = tdArr[5].text # 已累計時間
+        logger.info(f"courseName : {courseName}")
+        logger.info(f"accmulateTime : {accmulateTime}")
         accmulateSecs = convertToSecs(accmulateTime) # 轉換成 second
-        if accmulateSecs < convertToSecs("01:00:00"): # 累計時數1小時以下的才去掛課
+        if accmulateSecs < convertToSecs(targetHHmmss): # 累計時數1小時以下的才去掛課
             goToCourseStr = curseTr.get_attribute("onclick")
             courseId = re.search("\(([^)]+)\)", goToCourseStr).group(1) # 取得括號中的courseId → ex: gotoCourse("10041")
             needSecs = convertToSecs(targetHHmmss) - convertToSecs(accmulateTime) # 需多少時間(秒)
             courseList.append({ "courseName": courseName, "courseId": courseId, "needSecs": needSecs })
 
-    print("=========================================================")
-    print(f"@@@ 需要掛時間的課程 @@@ : {courseList}")
-    print("=========================================================")
+    logger.info("=========================================================")
+    logger.info(f"@@@ 需要掛時間的課程 @@@ : {courseList}")
+    logger.info("=========================================================")
 
     for idx, courseInfo in enumerate(courseList):
-        print(idx, courseInfo)
+        logger.info(idx, courseInfo)
         attendToCourse(browser, courseInfo, neededSecs=int(courseInfo.get("needSecs")))
-        print(f"課程「{courseInfo.get('courseName')}」結束!")
+        logger.info(f"課程『{courseInfo.get('courseName')}』結束!")
 
     browser.close()
